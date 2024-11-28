76e09d4069a1d3051838499dfe7a952c0a0e022f
Cristian Aragón <crisarag8.sal@gmail.com>
2024-11-28 01:05:24 -0600

    Cambio de nombre de la empresa

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import './Home.css';
 const Home = () => {
     return (
         <div className="home-content">
-            <h1>Bienvenido a Mi Empresa</h1>
+            <h1>Bienvenido a Pollos "El Güero"</h1>
             <p>Ofrecemos los mejores servicios para ti. ¡Contáctanos!</p>
             <button className="btn-primary">Explorar</button>
         </div>
