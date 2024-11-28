1c918b32fa536f076492d08ab663f8481ba2062f
Cristian Aragón <crisarag8.sal@gmail.com>
2024-11-28 16:40:36 -0600

    Implementación del swiper en home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import './Home.css';
-import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import Swiper from './Swiper';
+import menu from '../assets/data/menu.json';
 
 const Home = () => {
     return (
         <div className="home-content">
             <h1>Bienvenido a Pollos "El Güero"</h1>
             <p>Ofrecemos los mejores servicios para ti. ¡Contáctanos!</p>
-            <Swiper
-                spaceBetween={30}
-                slidesPerView={1}
-                loop={true}
-                autoplay={{
-                    delay: 3000,
-                    disableOnInteraction: false,
-                }}
-            >
-                <SwiperSlide><div className="slide">Servicio 1</div></SwiperSlide>
-                <SwiperSlide><div className="slide">Servicio 2</div></SwiperSlide>
-                <SwiperSlide><div className="slide">Servicio 3</div></SwiperSlide>
-            </Swiper>
             <button className="btn-primary">Explorar</button>
+            <Swiper slides={menu} />
         </div>
     );
 };
