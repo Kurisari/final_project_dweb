78d986d31740c60bdce7f6188128ae0427d70896
Cristian Aragón <crisarag8.sal@gmail.com>
2024-11-28 01:41:44 -0600

    Implementación de swiper e imagen de inicio

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './Home.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 const Home = () => {
     return (
         <div className="home-content">
             <h1>Bienvenido a Pollos "El Güero"</h1>
             <p>Ofrecemos los mejores servicios para ti. ¡Contáctanos!</p>
+            <Swiper
+                spaceBetween={30}
+                slidesPerView={1}
+                loop={true}
+                autoplay={{
+                    delay: 3000,
+                    disableOnInteraction: false,
+                }}
+            >
+                <SwiperSlide><div className="slide">Servicio 1</div></SwiperSlide>
+                <SwiperSlide><div className="slide">Servicio 2</div></SwiperSlide>
+                <SwiperSlide><div className="slide">Servicio 3</div></SwiperSlide>
+            </Swiper>
             <button className="btn-primary">Explorar</button>
         </div>
     );
