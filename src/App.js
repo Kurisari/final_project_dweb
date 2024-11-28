95a8521668e31adf540c6a62d47323eb9630001b
Cristian Aragón <crisarag8.sal@gmail.com>
2024-11-28 15:41:26 -0600

    Implementación de menú con carrusel

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import Swiper from './components/Swiper';
+import menu from './assets/data/menu.json';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Swiper slides={menu} />
       <Footer />
     </div>
   );
