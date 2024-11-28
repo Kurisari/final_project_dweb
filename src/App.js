2da41a35c6b8734c457ff3bbf708ef62b54e09b4
Cristian Aragón <crisarag8.sal@gmail.com>
2024-11-28 16:40:17 -0600

    Eliminación del swiper

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import Swiper from './components/Swiper';
-import menu from './assets/data/menu.json';
+// import menu from './assets/data/menu.json';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
-      <Swiper slides={menu} />
       <Footer />
     </div>
   );
