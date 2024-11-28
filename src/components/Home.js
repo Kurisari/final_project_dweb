2c8b452c12bc01cfaf85512494bbf6542f1ce5d5
Cristian Aragón <crisarag8.sal@gmail.com>
2024-11-28 00:39:11 -0600

    Creación de coponente home

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className="home-content">
+            <h1>Bienvenido a Mi Empresa</h1>
+            <p>Ofrecemos los mejores servicios para ti. ¡Contáctanos!</p>
+            <button className="btn-primary">Explorar</button>
+        </div>
+    );
+};
+
+export default Home;
