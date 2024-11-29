f53937952196507fc7a566e16708a57496072147
Cristian Aragón <crisarag8.sal@gmail.com>
2024-11-28 19:40:28 -0600

    Cambio en la presentación de título

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,9 +7,13 @@ import menu from '../assets/data/menu.json';
 const Home = () => {
     return (
         <div className="home-content">
-            <h1>Bienvenido a Pollos "El Güero"</h1>
-            <p>Ofrecemos los mejores servicios para ti. ¡Contáctanos!</p>
-            <button className="btn-primary">Explorar</button>
+            <header>
+                <div className="container">
+                    <h1>Bienvenido a Pollos "El Güero"</h1>
+                    <p>Ofrecemos los mejores servicios para ti. ¡Contáctanos!</p>
+                    <button className="btn-primary">Explorar</button>
+                </div>
+            </header>
             <Swiper slides={menu} />
         </div>
     );
