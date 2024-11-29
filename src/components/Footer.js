78e65a58ae3551834958689497d8008d75c87b77
Cristian Aragón <crisarag8.sal@gmail.com>
2024-11-29 11:39:12 -0600

    Actualización para dar crédito a los desarrolladores

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>&copy; {new Date().getFullYear()} Pollos "El Güero". Todos los derechos reservados.</p>
+            <p>&copy; {new Date().getFullYear()} Pollos "El Güero". Todos los derechos reservados.<br></br>Desarrollado por Cristian Aragón y Luis Fernando de Anda</p>
         </footer>
     );
 };
