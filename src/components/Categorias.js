ad564cd593ef5efe68bd4414397105d2f72ac70e
Cristian Aragón <crisarag8.sal@gmail.com>
2024-11-29 17:29:25 -0600

    Página dedicada a categorías creada

---
+++ src/components/Categorias.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Categorias.css'; // Crea este archivo para estilos específicos
+
+const Categorias = () => {
+    return (
+        <div className="categorias">
+            <h2>Categorías de Productos</h2>
+            <p>Explora nuestras diferentes categorías de productos.</p>
+            {/* Agrega contenido de categorías aquí */}
+        </div>
+    );
+};
+
+export default Categorias;
