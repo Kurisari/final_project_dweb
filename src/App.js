95b9bfe4a39d5b5de001b27608ec40c71942bb6d
Cristian Aragón <crisarag8.sal@gmail.com>
2024-11-29 17:28:41 -0600

    Primera versión de control de páginas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import AcercaDe from './components/AcercaDe';
+import Categorias from './components/Categorias';
+import Carrito from './components/Carrito';
 
 function App() {
   const [cartItems, setCartItems] = useState(0);
@@ -12,11 +16,16 @@ function App() {
     setCartItems(cartItems + 1);
   };
   return (
-    <div className="App">
+    <Router>
       <Header cartItems={cartItems}/>
-      <Home addToCart={addToCart}/>
+      <Routes>
+        <Route path='/' element={<Home addToCart={addToCart} />} />
+        <Route path="/acerca-de" element={<AcercaDe />} />
+        <Route path="/categorias" element={<Categorias />} />
+        <Route path="/carrito" element={<Carrito />} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
