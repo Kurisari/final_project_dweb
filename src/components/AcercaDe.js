25a42380d54bfe66f8c2f27e110b8eddef67e3fa
Cristian Aragón <crisarag8.sal@gmail.com>
2024-11-29 17:28:57 -0600

    Página de acerca de creada

---
+++ src/components/AcercaDe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './AcercaDe.css'; // Crea este archivo para estilos específicos
+
+const AcercaDe = () => {
+    return (
+        <div className="acerca-de">
+            <h2>Acerca de Nosotros</h2>
+            <p>Bienvenido a Pollos "El Güero", tu mejor opción en pollos rostizados.</p>
+            {/* Más contenido aquí */}
+        </div>
+    );
+};
+
+export default AcercaDe;
